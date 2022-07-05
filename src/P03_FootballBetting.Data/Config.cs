ceee9dcbda1e66b79b351515bf4294aaf592c831
KrIsKa7a <kristiqn.ivanov@abv.bg>
2022-07-05 21:54:30 +0300

    Remove Config.cs

--- src/P03_FootballBetting.Data/Config.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace P03_FootballBetting.Data
-{
-    public static class Config
-    {
-        // ReSharper disable once InconsistentNaming
-        public const string ConnectionString = 
-            @"Server=DESKTOP-2ASBARL\SQLEXPRESS;Database=Bet377;Integrated Security=True;";
-    }
-}
